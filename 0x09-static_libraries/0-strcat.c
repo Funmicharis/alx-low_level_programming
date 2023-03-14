c9e3164abea242e488a36b4d8be7436e7fd56744
funmicharis <funmiojo19@gmail.com>
2023-03-13 20:26:53 -0700

    Update

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -6,11 +6,10 @@
  *
  * Return: void
  */
-	char *_strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
 	int i;
 	int j;
-
 
 	i = 0;
 	while (dest[i] != '\0')
@@ -21,9 +20,8 @@
 	while (src[j] != '\0')
 {
 		dest[i] = src[j];
-		i++;
-		j++;
-	}
+i++;
+j++;
 
 	dest[i] = '\0';
 	return (dest);
